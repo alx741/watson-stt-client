a40fa83bafee4e41a5c37613cb69f989f54d5a8e
Daniel Campoverde <alx@sillybytes.net>
2019-05-15 19:19:55 -0500

    Access token retrieval script

---
+++ get_access_token.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -k -X POST --header "Content-Type: application/x-www-form-urlencoded" \
+    --header "Accept: application/json" \
+    --data-urlencode "grant_type=urn:ibm:params:oauth:grant-type:apikey" \
+    --data-urlencode "apikey=$(cat api_key)" \
+    "https://iam.bluemix.net/identity/token"
