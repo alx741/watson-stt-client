ac1b48f67a5f935b9897ce816dc8f7efea5b7c62
Daniel Campoverde <alx@sillybytes.net>
2019-05-16 18:05:34 -0500

    Fix RecognitionRequest ToJSON instance

--- src/Types.hs
+++ src/Types.hs
@@ -1,19 +1,31 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Types
     ( startRecognitionReq
+    , stopRecognitionReq
     )
     where
 
-import GHC.Generics (Generic)
 import Data.Aeson
+import Data.Maybe   (fromMaybe)
+import Data.Text    (Text)
+import GHC.Generics (Generic)
 
 startRecognitionReq :: RecognitionRequest
-startRecognitionReq = RecognitionRequest "start" "audio/wav"
+startRecognitionReq = RecognitionRequest "start" $ Just "audio/wav"
+
+stopRecognitionReq :: RecognitionRequest
+stopRecognitionReq = RecognitionRequest "stop" Nothing
 
 data RecognitionRequest = RecognitionRequest
-    { action      :: String
-    , contentType :: String
+    { action      :: Text
+    , contentType :: Maybe Text
     } deriving (Show, Eq, Generic)
 
-instance ToJSON RecognitionRequest
+instance ToJSON RecognitionRequest where
+    toJSON (RecognitionRequest action mContentType) =
+        object
+            [ "action" .= action
+            , "content-type" .= fromMaybe "" mContentType
+            ]
