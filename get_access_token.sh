b7adb1a111b705d12cc7d62c9c1127913d623e80
Daniel Campoverde <alx@sillybytes.net>
2019-05-18 20:04:19 -0500

    get_access_token takes the token automatically

--- get_access_token.sh
+++ get_access_token.sh
@@ -4,4 +4,5 @@ curl -k -X POST --header "Content-Type: application/x-www-form-urlencoded" \
     --header "Accept: application/json" \
     --data-urlencode "grant_type=urn:ibm:params:oauth:grant-type:apikey" \
     --data-urlencode "apikey=$(cat api_key)" \
-    "https://iam.bluemix.net/identity/token"
+    "https://iam.bluemix.net/identity/token" \
+    | cut -d'"' -f4 > access_token
