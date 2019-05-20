ff3244af53a84b274558a1ffff1dc8d9b30b2f8c
Daniel Campoverde <alx@sillybytes.net>
2019-05-20 17:56:45 -0500

    Refactor to take a Conduit pipeline

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
+import Control.Monad (forever)
+import Conduit
+import Data.Text (Text)
 import Client
 
 config :: ClientConfig
 config = ClientConfig "es-ES_BroadbandModel" 0.1
 
 main :: IO ()
-main = run config "access_token"
+main = runStreamWithSpeech config "access_token" sink
+
+sink :: ConduitT Text Void IO ()
+sink = awaitForever $ liftIO . print
 
 -- arecord -f S16_LE -c1 -r 16000 -t raw | stack exec client
--- src/Client.hs
+++ src/Client.hs
@@ -2,19 +2,22 @@
 
 module Client where
 
+import           Conduit
 import           Control.Concurrent   (forkIO, threadDelay)
 import           Control.Monad        (forever)
 import           Data.Aeson           (ToJSON, decode)
 import           Data.Aeson.Text      (encodeToLazyText)
 import qualified Data.ByteString.Lazy as LBS (hGet)
-import           Data.Text            as T (Text, unpack)
-
-import Network.WebSockets (ClientApp, Connection, receiveData, sendBinaryData,
-                           sendClose, sendTextData)
-import System.IO          (hFlush, hPrint, stderr, stdin, stdout)
-import Wuss               (runSecureClient)
+import           Data.Text            as T (Text)
+import           Network.WebSockets   (ClientApp, receiveData, sendBinaryData,
+                                       sendClose, sendTextData)
+import           System.IO            (hPrint, stderr, stdin)
+import           Wuss                 (runSecureClient)
 
 import Types
+
+
+type Pipeline = ConduitT Text Void IO ()
 
 data ClientConfig = ClientConfig
     { model                  :: String -- ^ e.g. "es-ES_BroadbandModel
@@ -29,49 +32,48 @@ uri accessToken m = "/speech-to-text/api/v1/recognize"
     <> "?access_token=" <> accessToken
     <> "&model=" <> m
 
-run :: ClientConfig -> FilePath ->  IO ()
-run (ClientConfig m s) token =
+runStreamWithSpeech :: ClientConfig -> FilePath -> Pipeline -> IO ()
+runStreamWithSpeech (ClientConfig m s) token pipeline =
     filter (/= '\n') <$> readFile token
     >>= (\accessToken ->
-        runSecureClient host 443 (uri accessToken m) (app s))
+        runSecureClient host 443 (uri accessToken m) (app s pipeline))
 
-sendStdinRaw :: Connection -> Int -> IO ()
-sendStdinRaw conn bytes = do
-    raw <- LBS.hGet stdin bytes
-    sendBinaryData conn raw
-    sendTextData conn $ encodeToLazyText stopRecognitionReq
-
-app :: Float -> ClientApp ()
-app seconds conn = do
+app :: Float -> Pipeline -> ClientApp ()
+app seconds pipeline conn = do
     putStrLn "-- Connected"
-
-    -- Send audio data
-    startRequest conn
-    _ <- forkIO $ forever $ sendStdinRaw conn $ floor $ 16.0 * 16000.0 * seconds
-
-    -- Recive answers
-    _ <- forever $ do
-            rawResponse <- receiveData conn
-
-            case decode rawResponse :: Maybe RecognitionResults of
-                Just result -> do
-                    putStr $ unpack $ prettyResult result
-                    hFlush stdout
-                Nothing -> pure ()
-
-            case decode rawResponse :: Maybe ErrorResponse of
-                -- Restart upon error
-                Just (ErrorResponse e) -> startRequest conn >> hPrint stderr e
-                Nothing -> pure ()
-
-    -- FIXME: When to close the connection?
+    startRequest
+    _ <- forkIO $ forever $ sendStdinRaw $ floor $ 16.0 * 16000.0 * seconds
+    _ <- runConduit $ speechSource .| pipeline
     sendClose conn ("" :: Text)
+    where
+        speechSource :: ConduitT () Text IO ()
+        speechSource = forever $ yieldM receiveTranscripts
 
-startRequest :: Connection -> IO ()
-startRequest conn = request startRecognitionReq conn >> threadDelay 50000
+        sendStdinRaw :: Int -> IO ()
+        sendStdinRaw bytes = do
+            raw <- LBS.hGet stdin bytes
+            sendBinaryData conn raw
+            stopRequest
 
-stopRequest :: Connection -> IO ()
-stopRequest = request stopRecognitionReq
+        receiveTranscripts :: IO Text
+        receiveTranscripts = do
+            rawResponse <- receiveData conn
+            case decode rawResponse :: Maybe RecognitionResults of
+                Just result -> pure $ prettyResult result
+                Nothing ->
+                    case decode rawResponse :: Maybe ErrorResponse of
+                        -- Restart upon error
+                        Just (ErrorResponse e) -> do
+                            startRequest
+                            hPrint stderr e
+                            receiveTranscripts
+                        Nothing -> receiveTranscripts
 
-request :: ToJSON a => a -> Connection -> IO ()
-request req conn = sendTextData conn $ encodeToLazyText req
+        startRequest :: IO ()
+        startRequest = request startRecognitionReq >> threadDelay 50000
+
+        stopRequest :: IO ()
+        stopRequest = request stopRecognitionReq
+
+        request :: ToJSON a => a -> IO ()
+        request = sendTextData conn . encodeToLazyText
